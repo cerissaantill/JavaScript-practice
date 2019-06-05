8d3cf900fbda3487cab4b3a0e586b0a64dc71906
Cerissa Antill <cerissa.antill@gmail.com>
2019-06-05 15:36:53 -0500

    cleaned up

--- konami/konami.js
+++ konami/konami.js
@@ -2,7 +2,7 @@
 
 (function(){
     "use strict";
-    let konami = [84, 79, 66, 89, 73, 83, 66, 65, 67, 75],
+    let konami = [84, 79, 66, 89, 73, 83, 66, 65, 67, 75],  // T O B Y I S B A C K
         pressedKey,
         index = 0,
         started = false;
@@ -12,33 +12,28 @@
     $(document).keyup(function(e){
         console.log(e.keyCode);
 
-        //Here we are creating a reset function that is triggered when the array needs to be reset.
-
+        //This is creating a reset function that is triggered when the array needs to be reset.
         function reset() {
             index = 0;
             started = false;
         }
 
         //Below is the function that initiates when all 10 keys have been pressed in the right order.
-
-        let url = "https://giphy.com/gifs/michael-scott-KajpERDjOpMcw/fullscreen";
+        let url = "url-address-here";
         function konamiComplete() {
             $(location).attr('href',url);
             console.log("worked");
         }
 
         //Next, we are going to declare a variable for the user's inputted number.
-
         pressedKey = e.keyCode;
 
         //conditionals to check whether or not the first key is the right key. If not then the array gets reset.
-
         if (pressedKey === 84) {
             started = true;
         }
 
         //Once the first key is pressed, we are good to continue with the array conditionals.
-
         if(started){
             if (konami[index] === pressedKey) {
                 index++;
