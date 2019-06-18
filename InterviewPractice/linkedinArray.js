bf0f7d0adc3809d0a37065536bb35810def612b8
Cerissa Antill <cerissa.antill@gmail.com>
2019-06-18 15:27:41 -0500

    setup array Q from Linkedin Learning

---
+++ InterviewPractice/linkedinArray.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// LinkedIn Learning
+// Mastering Web Developer Interview Code
+
+
+var arr = [
+    { name: "LinkedIn", link:"http://linkedin.com/in/planetoftheweb" },
+    { name: "Blog", link:"http://raybo.org" },
+    { name: "Twitter", link: "http://twitter.com/planetoftheweb" }
+];
+
+
+// 1. Display this list in HTML
+// 2. Target id of "myLinks"
