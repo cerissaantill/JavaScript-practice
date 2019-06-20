64fe0e64649f5bf64a0eab5306c1bee1f0fa9fe3
Cerissa Antill <cerissa.antill@gmail.com>
2019-06-20 10:50:41 -0500

    added answer to stockPrices

--- Int_practice_larger_problems/stockPrices.js
+++ Int_practice_larger_problems/stockPrices.js
@@ -15,3 +15,19 @@
 // HINT: start by splitting the String and looping through
 // important parts: string concat &
 // += does the operation & assignment at once
+
+
+// define stock prices
+let stockPrices = '4, 3, 10, 9, 5, 1, 6';
+
+function printPrices(prices) {
+    let pricesArray = prices.split(',');
+    for (let i = 0; i <= pricesArray.length; i++) {
+        for (let j = 0; j <= pricesArray[i]; j++) {
+            // build the dashes here
+
+        }
+        console.log(pricesArray.join(\'-'\));
+        // console.log(`${i}: dashes here`);
+    }
+}
