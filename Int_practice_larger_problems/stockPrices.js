0e665862eedefe3c1d02049f4bf54bfba3c96647
Cerissa Antill <cerissa.antill@gmail.com>
2019-06-20 10:49:13 -0500

    added stockPrices problem

---
+++ Int_practice_larger_problems/stockPrices.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+// Practice: Take a comma-delimited string of numbers (stock prices), and print a chart, where each line starts with the time index (array index), colon, then a number of dashes in proportion to the price.
+// Example input:
+// 4,3,10,9,5,1,6
+// 0:----
+// 1:---
+// 2:----------
+// 3:---------
+// 4:-----
+// 5:-
+// 6:------
+
+// HINT: start by splitting the String and looping through
+// important parts: string concat &
+// += does the operation & assignment at once
