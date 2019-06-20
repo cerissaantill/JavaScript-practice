d673e22170ad33e4a727430c7f99046159b96e6f
Cerissa Antill <cerissa.antill@gmail.com>
2019-06-20 10:52:12 -0500

    added charStringCount

---
+++ Int_practice_larger_problems/charStringCount.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+// Practice: Take a string and return that string, but with a number after each
+// character, denoting the number of times it appears in the string.
+// Example input:
+// hello, how are you?
+// Example output:
+// h2e2l2,1 3h2o3w1 3a1r1e2 3y1o3u1?1
