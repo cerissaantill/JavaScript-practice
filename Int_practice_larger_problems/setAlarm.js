52d0ac785c72e6390266a1e27e861a48b84a73d7
Cerissa Antill <cerissa.antill@gmail.com>
2019-06-25 13:51:39 -0500

    completed setAlarm challenge

---
+++ Int_practice_larger_problems/setAlarm.js
@@ -0,0 +1,49 @@
+'use strict';
+
+
+/**
+ CODEWARS 6/25/19
+
+ Write a function named setAlarm which receives two parameters.
+ The first parameter, employed, is true whenever you are employed and
+ the second parameter, vacation, is true whenever you are on vacation.
+
+
+ The function should return true if you are employed and not on vacation
+ (because these are the circumstances under which you need to set an alarm).
+ It should return false otherwise.
+ */
+
+
+
+function setAlarm(isEmployed, onVacation) {
+
+    if (isEmployed === true && onVacation === true) {
+        return false;
+    } else {
+        if (isEmployed === false && onVacation === false) {
+            return false;
+        } else {
+            if (isEmployed === false && onVacation === true) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
+console.log(setAlarm());
+
+
+
+
+// Refactored:
+
+function setAlarm2(isEmployed, onVacation) {
+    if (isEmployed === true && onVacation === false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(setAlarm2());
